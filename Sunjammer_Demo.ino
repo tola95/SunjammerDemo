4039473dafdf2da5b05e1d2efbc83194152215c9
Omotola Babasola <otb13@ic.ac.uk>
2015-08-04 15:13:23 +0100

    Added a couple of commands to the main loop and included some housekeeping information

--- Sunjammer_Demo.ino
+++ Sunjammer_Demo.ino
@@ -1,6 +1,7 @@
 //SPI is an arduino library that lets you communicate withh SPI devices`
 #include <SPI.h>
 #include <stdio.h>
+//#include <pthread.h>
 
 //C:\WinAVR-20100110\bin\avrdude.exe -P COM -c arduino -p at90usb1286 -U flash:w:Sunjammer_FAS_0_2_2.cpp.hex:i
 
@@ -9,6 +10,12 @@
 #define SPI_CLK    PIN_B1 //Serial clock
 #define SPI_MOSI   PIN_B2 //Master Out Slave In
 #define SPI_MISO   PIN_B3 //Master In Slave Out
+
+#define VCC5V 2
+#define REF5V 3
+
+#define OB_RANGE PIN_F0
+#define IB_RANGE PIN_F1
 
 //Constants
 const int baudRate = 9600;
@@ -19,10 +26,22 @@ const int bufferCapacity = 256;
 //Global Variables
 int counter = 0; //Number times loop is executed
 int input;
+bool telementary = true;
+bool graphicalDisplay = false;
+int vectorCount = 0;
+//pthread_mutex_t buffer_lock =PTHREAD_MUTEX_INITIALIZER;
+
+// Status values
+int mode = 1;
+int packet_count;
+byte command_count;
+unsigned int vcc_5v;
+unsigned int ref_5v;
 
 //Struct representing a vector
 struct vector
 {
+  int id;           //Id of this vector
   unsigned int obx; //Outboard x coordinate
   unsigned int oby; //Outboard y coordinate
   unsigned int obz; //Outboard z coordinate
@@ -48,7 +67,21 @@ void setup() {
   
   Serial.begin(baudRate);
   Serial.flush();
-  Serial.println("Readings from the Magnetometer");
+  Serial.println("  -------------------------------------- ");
+  Serial.println("  |  Sunjammer MAGIC Flight Software    |");
+  Serial.println("  |  Version 0.0.1                      |");
+  Serial.println("  |  2015-08-03                         |");
+  Serial.println("  |  (C) Space Magnetometer Laboratory  |");
+  Serial.println("  |  Imperial College London            |");
+  Serial.println("  ---------------------------------------");
+  Serial.println("                Commands              ");
+  Serial.println("           r - Read from buffer       ");
+  Serial.println("           k - Read hosekeeping data  ");
+  Serial.println("           h - Help  ");
+  Serial.println("           o - Telementary on  ");
+  Serial.println("           f - Telementary off ");
+  Serial.println("           g - Graphical Telementary on  ");
+  Serial.println("           n - Graphical Telementary off ");
 
   //Initialise the necessary pins
   pinMode(SPI_SS, OUTPUT);
@@ -61,17 +94,44 @@ void setup() {
 }
 
 void loop() {
-  buffer_write(readRawValues());
+  buffer_write(readVector());
   
   input = Serial.read();
-  if (input == (int) 'r') {
-    buffer_read ();
+  switch (input) {
+    case ((int) 'r') :
+      buffer_read();
+      telementary = false;
+      Serial.println(" Press o to turn telementary back on ");
+      break;
+    case ((int) 'k') :
+      showHousekeeping();
+      telementary = false;
+      Serial.println(" Press o to turn telementary back on ");
+      break;
+    case ((int) 'h') :
+      help();
+      telementary = false;
+      Serial.println(" Press o to turn telementary back on ");
+      break;
+    case ((int) 'o') :
+      telementary = true;
+      break;
+    case ((int) 'f') :
+      telementary = false;
+      break;
+    case ((int) 'g') :
+      graphicalDisplay = true;
+      break;
+    case ((int) 'n') :
+      graphicalDisplay = false;
+      break;
+    default : ;
   }
   
-  delay(1000/16);
+  delay(1000/16); //Dalay for 1/16 of a second
 }
 
-struct vector readRawValues(){
+struct vector readVector(){
   struct vector vec;
 
   //Enable SPI reading
@@ -89,27 +149,25 @@ struct vector readRawValues(){
   vec.iby = getSPIValue(6);
   vec.ibz = getSPIValue(7);
   vec.ibt = getSPIValue(0);
+  vec.id = vectorCount;
 
   //Disable SPI reading
   digitalWrite(SPI_SS,HIGH);
   SPI.endTransaction();
 
   //Print the readings from the vector
-  //printVector(vec);
+  if (telementary) {
+    if (graphicalDisplay) {
+      printGraphicalVector(vec);
+    } else {
+      printVector(vec);
+    }
+  }
 
+  vectorCount++;
   return vec;
 }
 
-//Print the vector to Serial Monitor
-void printVector(struct vector vec) {
-  char tbs[64];
-  sprintf(tbs, "%03X %03X %03X %03X   %03X %03X %03X %03X", 
-          vec.obx,vec.oby,vec.obz,vec.obt,vec.ibx,vec.iby,vec.ibz,vec.ibt);
-  Serial.println(tbs);
-  
-}
-
-//Read the 
 unsigned int getSPIValue(int inputChannel) {
   
   /* The ADC 128S022 uses bits 5,4 and 3 to select channels hence the '<<3' convention
@@ -127,69 +185,30 @@ unsigned int getSPIValue(int inputChannel) {
   return coordinate;
 }
 
-void showAverage() {
-  struct vector vec;
-  
-  vec.obx = buffer_average(1);
-  vec.oby = buffer_average(2);
-  vec.obz = buffer_average(3);
-  vec.obt = buffer_average(4);
-  vec.ibx = buffer_average(5);
-  vec.iby = buffer_average(6);
-  vec.ibz = buffer_average(7);
-  vec.ibt = buffer_average(0);
+void showHousekeeping() {
 
-  //printVector(vec);
-  printGraphicalVector(vec);
+  Serial.println("         STATUS");
+  Serial.print("    Time (ms)      : "); Serial.println(millis());
+  Serial.print("    Mode           : "); Serial.println(mode);
+  Serial.print("    Outboard Range : "); 
+  if (analogRead(OB_RANGE)==0) {Serial.println("LOW");} else {Serial.println("HIGH");}
+  Serial.print("    Inboard Range  : "); 
+  if (analogRead(IB_RANGE)==0) {Serial.println("LOW");} else {Serial.println("HIGH");}
+  Serial.print("    Packet Count   : "); Serial.println(packet_count);
+  Serial.print("    VCC 5V         : "); Serial.println(analogRead(VCC5V));
+  Serial.print("    REF 5V         : "); Serial.println(analogRead(REF5V));
+
 }
 
-uint16_t buffer_average(int selector) {
-  uint16_t sum = 0;
-  
-  switch (selector) {
-    case 1:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].obx;
-      }
-      break;
-    case 2:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].oby;
-      }
-      break;
-    case 3:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].obz;
-      }
-      break;
-    case 4:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].obt;
-      }
-      break;
-    case 5:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].ibx;
-      }
-      break;
-    case 6:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].iby;
-      }
-      break;
-    case 7:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].ibz;
-      }
-      break;
-    case 0:
-      for (int i=0; i<bufferCapacity; i++) {
-        sum += buf.storage[i].ibt;
-      }
-      break;
-    default: ;
-  }
-    return sum/bufferCapacity;
+void help() {
+  Serial.println("                Commands              ");
+  Serial.println("           r - Read from buffer       ");
+  Serial.println("           k - Read hosekeeping data  ");
+  Serial.println("           h - Help  ");
+  Serial.println("           o - Telementary on  ");
+  Serial.println("           f - Telementary off ");
+  Serial.println("           g - Graphical Telementary on  ");
+  Serial.println("           n - Graphical Telementary off ");
 }
 
 ///////////////////////////////////////
@@ -198,6 +217,7 @@ uint16_t buffer_average(int selector) {
 
 void buffer_write(struct vector vec) {
 
+  //pthread_mutex_lock(&buffer_lock);
   acquire_buffer_lock();
   if (buf.count < bufferCapacity) {
     for (int i = buf.count -1; i>=0; i--) {
@@ -215,20 +235,23 @@ void buffer_write(struct vector vec) {
     Serial.println("Buffer count error. Capacity Exceeded");
   }
   release_buffer_lock();
+  //pthread_mutex_unlock(&buffer_lock);
 }
 
 struct vector buffer_read () {
+  //pthread_mutex_lock(&buffer_lock);
   acquire_buffer_lock();
   struct vector vec = buf.storage[buf.count-1];
   buf.count--;
   release_buffer_lock();
-  printGraphicalVector(vec);
+  //pthread_mutex_unlock(&buffer_lock);
+  printVector(vec);
   return vec;
 }
 
+//Spin locks protecting buffer
 void acquire_buffer_lock() {
   while (buf.lock == 1) {
-    
   }
   buf.lock = 1;
 }
@@ -237,7 +260,13 @@ void release_buffer_lock() {
   buf.lock = 0;
 }
 
+////////////////////////////////////////////////
+////////////// PRINTING METHODS ////////////////
+////////////////////////////////////////////////
+
 void printGraphicalVector(struct vector vec) {
+  Serial.print(vec.id);
+  Serial.print("   ");
   Serial.print(graphical(vec.obx));
   Serial.print(" ");
   Serial.print(graphical(vec.oby));
@@ -268,3 +297,10 @@ String graphical(unsigned int coord) {
   return str;
 }
 
+//Print the vector to Serial Monitor
+void printVector(struct vector vec) {
+  char tbs[64];
+  sprintf(tbs, "%X)  %03X %03X %03X %03X   %03X %03X %03X %03X", 
+          vec.id,vec.obx,vec.oby,vec.obz,vec.obt,vec.ibx,vec.iby,vec.ibz,vec.ibt);
+  Serial.println(tbs);
+}
